90fec04d5bd7ff2a041f2651ed14a3b22696a355
Martin Amoah <martinamoah2002@gmail.com>
2025-08-26 21:49:24 +0000

    Error: Route "/dashboard" used `cookies().getAll()`. `cookies()` should

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,10 +1,9 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
   const cookieStore = cookies()
 
-  // Create a server-side client
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -13,18 +12,18 @@ export function createClient() {
         get(name: string) {
           return cookieStore.get(name)?.value
         },
-        set(name: string, value: string, options: CookieOptions) {
+        set(name: string, value: string, options) {
           try {
-            cookieStore.set({ name, value, ...options })
+            cookieStore.set(name, value, options)
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
         },
-        remove(name: string, options: CookieOptions) {
+        remove(name: string, options) {
           try {
-            cookieStore.set({ name, value: '', ...options })
+            cookieStore.set(name, '', options)
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
